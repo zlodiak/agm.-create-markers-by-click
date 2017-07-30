83f6fc0ca7a36da9574c19ab2b6779cbe6091af4
zlodiak <prozaik81-2@yandex.ru>
2017-07-30 15:31:27 +0300

    agm create markers example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  private mapClicked($event) {
+    this.markers.push({
+      lat: $event.coords.lat,
+      lng: $event.coords.lng
+    });
+
+    console.log(this.markers);
+  };  
+
+  markers: marker[] = [];  
+}
+
+interface marker {
+    lat: number;
+    lng: number;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { 	MdCardModule, 
+					MdButtonModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+
+import { AgmCoreModule } from '@agm/core';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+  	MdButtonModule,
+  	BrowserAnimationsModule,
+  	MdCardModule,
+    BrowserModule,
+  	AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyCcavO4Wf59cE1oyey7YkTnsSLGolOyNoI'
+    })    
   ],
   providers: [],
   bootstrap: [AppComponent]
